dc24a603a6fd146c95b09660c37b7ca805fa32df
BeeibekKunwar <beeibek098765@gmail.com>
2023-12-16 22:40:35 +0545

    Provider added that helps to select brand

--- lib/widgets/product_widgets/company_build.dart
+++ lib/widgets/product_widgets/company_build.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shoes_ui/models/brandModel.dart';
+import 'package:shoes_ui/provider/brand_build_provider.dart';
 import 'package:shoes_ui/utils/colors.dart';
 
 class BuildCompanyBuilder extends StatefulWidget {
@@ -10,7 +12,6 @@ class BuildCompanyBuilder extends StatefulWidget {
 }
 
 class _BuildCompanyBuilderState extends State<BuildCompanyBuilder> {
-  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -18,45 +19,50 @@ class _BuildCompanyBuilderState extends State<BuildCompanyBuilder> {
       child: SizedBox(
         height: 80,
         child: ListView.builder(
-            itemCount: products.length,
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, index) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-                child: SizedBox(
-                  width: 100,
-                  child: Card(
-                    elevation: 2,
-                    color: selectedIndex == index ? selectedColor : Colors.white,
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 2.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset(
-                                  products[index]['companyImageUrl'],
-                                  fit: BoxFit.contain,
+          itemCount: products.length,
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, index) {
+            return Consumer<BrandBuildProvider>(
+              builder: (BuildContext context, value, child) {
+                return GestureDetector(
+                  onTap: () {
+                    // value.read<BrandBuildProvider>().selectBrand();
+                    value.selectBrand(index);
+                  },
+                  child: SizedBox(
+                    width: 100,
+                    child: Card(
+                      elevation: 2,
+                      color: value.selectedIndex == index
+                          ? selectedColor
+                          : Colors.white,
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 2.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.asset(
+                                    products[index]['companyImageUrl'],
+                                    fit: BoxFit.contain,
+                                  ),
                                 ),
                               ),
-                            ),
-                      
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              );
-            }),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
